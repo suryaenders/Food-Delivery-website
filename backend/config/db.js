cfe512012bd4e71dd206095341385a785bc7f11a
suryaenders <suryanallasanthala666@gmail.com>
2026-06-10 19:50:31 +0530

    added db

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb://suryaenders:Surya12345@ac-1j6ldsn-shard-00-00.ndgxwea.mongodb.net:27017,ac-1j6ldsn-shard-00-01.ndgxwea.mongodb.net:27017,ac-1j6ldsn-shard-00-02.ndgxwea.mongodb.net:27017/Food-Delivery-website?ssl=true&replicaSet=atlas-oi897e-shard-0&authSource=admin&appName=Cluster0",
-    );
+    await mongoose.connect(process.env.MONGO_URI);
 
     console.log("DB Connected");
   } catch (error) {
