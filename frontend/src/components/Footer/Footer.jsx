58a05fe2fa2bcddd8cd5b82bc3fe87c82b486956
suryaenders <suryanallasanthala666@gmail.com>
2026-06-10 22:17:27 +0530

    completed

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -20,7 +20,7 @@ const Footer = () => {
           </div>
         </div>
         <div className="footer-content-center">
-          <h2>CCOMPANY</h2>
+          <h2>COMPANY</h2>
           <ul>
             <li>Home</li>
             <li>About us</li>
